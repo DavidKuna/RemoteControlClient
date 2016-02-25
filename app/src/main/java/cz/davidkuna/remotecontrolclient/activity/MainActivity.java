1f381022949a05cef225499d4c5f07fae7e112a9
David Kuna <kuna@netdevelo.cz>
2016-02-25 23:28:25 +0100

    Add QR code scanner for settings

--- app/src/main/java/cz/davidkuna/remotecontrolclient/activity/MainActivity.java
+++ app/src/main/java/cz/davidkuna/remotecontrolclient/activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,10 +18,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
 import java.io.FileNotFoundException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import cz.davidkuna.remotecontrolclient.helpers.Settings;
 import cz.davidkuna.remotecontrolclient.view.GyroVisualizer;
 import cz.davidkuna.remotecontrolclient.R;
 import cz.davidkuna.remotecontrolclient.log.LogSource;
@@ -33,6 +37,7 @@ import cz.davidkuna.remotecontrolclient.socket.UDPListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Settings settings = new Settings();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +58,40 @@ public class MainActivity extends AppCompatActivity {
                 connect();
             }
         });
+
+        Button bScanQR = (Button) findViewById(R.id.scanQRcode);
+        bScanQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadSettingsFromQR();
+            }
+        });
     }
+
+    private void loadSettingsFromQR() {
+        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+        intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+        startActivityForResult(intent, 0);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                Gson gson = new Gson();
+                settings = gson.fromJson(contents, Settings.class);
+                connect();
+                
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+                Toast toast = Toast.makeText(this, "Scan was Cancelled!", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.TOP, 25, 400);
+                toast.show();
+
+            }
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -83,14 +121,22 @@ public class MainActivity extends AppCompatActivity {
     private boolean connect() {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
+        String serverAddress;
+        if (!settings.getServerAddress().isEmpty()) {
+            serverAddress = settings.getServerAddress();
+        } else {
+            serverAddress = prefs.getString("server_ip", "127.0.0.1");
+        }
+
         final int interval = Integer.valueOf(prefs.getString("request_interval", "200")); // miliseconds
         InetAddress serverIp;
         try {
-            serverIp = InetAddress.getByName(prefs.getString("server_ip", "127.0.0.1"));
+            serverIp = InetAddress.getByName(serverAddress);
         } catch (UnknownHostException e) {
             Toast.makeText(this, "Unknown server host", Toast.LENGTH_SHORT).show();
             return false;
         }
+
 
         Intent intent = new Intent();
         intent.putExtra(ControlActivity.KEY_SERVER_ADDRESS, serverIp);
